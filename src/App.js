2f72c287c0eb35219af63d2eca0172c9f9e435e5
ydnaod <65431123+ydnaod@users.noreply.github.com>
2020-11-20 09:26:39 -0500

    add files from setup

--- src/App.js
+++ src/App.js
@@ -3,22 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+  <h1>Ja<span class="highlight">mmm</span>ing</h1>
+  <div class="App">
+    <!-- Add a SearchBar component -->
+    <div class="App-playlist">
+      <!-- Add a SearchResults component -->
+      <!-- Add a Playlist component -->
     </div>
+  </div>
+</div>
   );
 }
 
